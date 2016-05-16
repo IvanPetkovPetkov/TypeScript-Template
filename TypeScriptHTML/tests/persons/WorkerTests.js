1e95517816fd8b56f6a12f62e76a3b1aa4d421f5
Ivan Petkov <Ivna_Petkov@epam.com>
2016-05-16 15:31:09 +0300

    Add chai

--- TypeScriptHTML/tests/persons/WorkerTests.js
+++ TypeScriptHTML/tests/persons/WorkerTests.js
@@ -1,16 +1,6 @@
-/// <reference path="../../typings/mocha/mocha.d.ts" />
-///// <reference path="../../Scripts/typings/assert/assert.d.ts" />
-//var assertRequire = require('assert');
-describe("Test Suite 1", function () {
-    it("Test A", function () {
-        var resultBool = true;
-        it('should add two numbers together', function () {
-            var result = 2 + 3;
-            if (result !== 5) {
-                throw new Error('Expected 2 + 3 = 5 but was ' + result);
-            }
-        });
-        //assert.equal(true, this.resultBool, 'Not true');
-    });
-});
+///// <reference path="../../typings/mocha/mocha.d.ts" />
+//describe('Calculator', () => {
+//    it('should add two numbers', () => {
+//    });
+//});
 //# sourceMappingURL=WorkerTests.js.map
--- TypeScriptHTML/tests/persons/WorkerTests.ts
+++ TypeScriptHTML/tests/persons/WorkerTests.ts
@@ -1,19 +1,8 @@
-﻿/// <reference path="../../typings/mocha/mocha.d.ts" />
-///// <reference path="../../Scripts/typings/assert/assert.d.ts" />
+﻿///// <reference path="../../typings/mocha/mocha.d.ts" />
 
-//var assertRequire = require('assert');
-
-describe("Test Suite 1", () => {
-    it("Test A", () => {
-        var resultBool: boolean = true;
-        it('should add two numbers together', () => {
-            var result: number = 2 + 3;
-            if (result !== 5) {
-                throw new Error('Expected 2 + 3 = 5 but was ' + result);
-            }
-        });
-        //assert.equal(true, this.resultBool, 'Not true');
-    });
-});
-
+//describe('Calculator', () => {
+//    it('should add two numbers', () => {
+        
+//    });
+//});
 
