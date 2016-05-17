68cd1a92524b44d620334bdc65a7cc23b550da17
Assen Illiev <assen_illiev@epam.com>
2016-05-17 16:51:10 +0300

    missed file

---
+++ TypeScriptHTML/Scripts/persons/ClientDecorator.ts
@@ -0,0 +1,11 @@
+﻿module Persons {
+    export class ClientDecorator extends Client  {
+        constructor(email: string, client: interfaces.persons.IIntroducable) {
+            super(client, email);
+        }
+
+        introduce(): string {
+            return super.introduce() + "`operation` of ClientDecorator", this.email, " is being called!"
+        }
+    }
+}
